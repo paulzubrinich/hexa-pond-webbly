1c7b4031f024256295331c0c0c34fdd1431c2bea
paulzubrinich <paulzubrinich@hotmail.com>
2017-12-26 01:58:32 +0100

    repaired touches hopefully

--- js/main.js
+++ js/main.js
@@ -415,13 +415,13 @@ function touchEnded() {
 
 function touchMoved() {
   if(touchPoints.length > 0 && touchPoints.length === touches.length) {
-    console.log(touches.length);
+    console.log(touchPoints.length);
     touchAffectsCloth = true;
     for(i = 0; i < touchPoints.length; i++) {
       touchPoints[i].px = touchPoints[i].x;
       touchPoints[i].py = touchPoints[i].y;
-      touchPoints[i].x = touches[1].x;
-      touchPoints[i].y = touches[1].y;
+      touchPoints[i].x = touches[i].x;
+      touchPoints[i].y = touches[i].y;
     }
   }
 }
